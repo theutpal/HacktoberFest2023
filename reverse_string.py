ebd7dd7e193f6afd9c1803ff6ef0a39e7fa90841
KallindSoni <kallindsoni@gmail.com>
2023-10-08 11:21:56 +0530

    Adding code to reverse a string

---
+++ reverse_string.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+#reverse a string function
+def reverse_string(string):
+    return string[::-1]
+#taking user input for string
+string = input("Enter a string: ")
+print(reverse_string(string))
